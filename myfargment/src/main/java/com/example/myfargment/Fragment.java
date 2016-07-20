61d68e170afa22962f7106cb79a37e42e6aaa1e0
931697478@qq.com <daxue920809>
2016-07-20 19:21:12 +0800

    碎片

---
+++ myfargment/src/main/java/com/example/myfargment/Fragment.java
@@ -0,0 +1,19 @@
+package com.example.myfargment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2016/7/20.
+ */
+public class Fragment extends android.support.v4.app.Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fargment, null);
+        return view;
+    }
+}
---
+++ myfargment/src/main/java/com/example/myfargment/Fragment2.java
@@ -0,0 +1,35 @@
+package com.example.myfargment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2016/7/20.
+ */
+public class Fragment2 extends android.support.v4.app.Fragment{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fargment2, null);
+
+
+
+        return view;
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
